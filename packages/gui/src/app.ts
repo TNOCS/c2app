047b9d2d41ad4050da6301a6de9dd68bf768e309
Timovdk <timo_kuil@hotmail.com>
2021-05-17 16:49:54 +0200

    PWA support and tsc compilation

--- packages/gui/src/app.ts
+++ packages/gui/src/app.ts
@@ -9,4 +9,21 @@ import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 
 import './styles.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/src/sw.js', { scope: '/src/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 m.route(document.body, routingSvc.defaultRoute, routingSvc.routingTable());
